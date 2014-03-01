747ae97535a6e7f5036ed8b106ba6b209b9dbb51
lyrobinson <lrobinson@teampraxis.com>
2014-02-28 16:02:11 -1000

    adding async version of ec2helper

--- ec2-helper-async.js
+++ ec2-helper-async.js
@@ -3,12 +3,16 @@
 
 var AWS = require('aws-sdk'),
     _ = require('lodash'),
-    nconf = require('nconf');
+    nconf = require('nconf'),
+    async = require('async');
 
 nconf.argv().env();
 
 var ec2 = new AWS.EC2({region: 'us-west-2'}),
-    reqNum = (nconf.get('_')[0] || nconf.get('NUM_EC2'));
+    reqNum = (nconf.get('_')[0] || nconf.get('NUM_EC2')),
+    instances,
+    startNum = 0,
+    makeNum = 0;
 
 var startEC2 = function(instances, startNum, callback) {
   var startInstances = _.chain(instances)
@@ -19,11 +23,10 @@ var startEC2 = function(instances, startNum, callback) {
   ec2.startInstances({InstanceIds: startInstances}, callback);
 };
 
-ec2.describeInstances(function(error, data) {
-  if (error) {
-    console.log('describeInstances error: ' + error);
-  } else {
-    var instances = _.chain(data.Reservations)
+async.waterfall([
+  _.bind(ec2.describeInstances, ec2),
+  function(data, callback) {
+    instances = _.chain(data.Reservations)
       .pluck('Instances')
       .flatten()
       .filter(function(i) {
@@ -38,33 +41,37 @@ ec2.describeInstances(function(error, data) {
     console.log(instances.length + " JMeter instances:\n ", instances);
     var instancesByState = _.countBy(instances, 'state');
     console.dir(instancesByState);
-
     if (reqNum <= ((instancesByState.running || 0) + (instancesByState.stopped || 0))) {
       if (reqNum <= (instancesByState.running || 0)) {
-        console.log('just enough running!');
-      } else {
+        callback('moof', instances, startNum);
+    } else {
         console.log('need to start ' + (reqNum - (instancesByState.running || 0)) + ' more');
-        startEC2(instances, (reqNum - (instancesByState.running || 0)), function(error, data) {
-          if (error) {
-            console.log('startInstances error: ' + error);
-          } else {
-            _.each(data.StartingInstances, function(instance) {
-              console.dir(instance);
-            });
-          }
-        });
+        startNum = reqNum - (instancesByState.running || 0);
+        callback(null, instances, startNum);
       }
     } else {
       console.log ('gotta start all the stopped ones and make ' + (reqNum - ((instancesByState.running || 0) + (instancesByState.stopped || 0))) + ' more');
-      startEC2(instances, (instancesByState.stopped || 0), function(error, data) {
-        if (error) {
-          console.log('startInstances error: ' + error);
-        } else {
-          _.each(data.StartingInstances, function(instance) {
-            console.dir(instance);
-          });
-        }
-      });
-    };
+      startNum = instancesByState.stopped || 0;
+      callback(null, instances, startNum);
+    }
+    console.log('startNum: ' + startNum);
+  },
+  startEC2,
+  function(data, callback) {
+    _.each(data.StartingInstances, function(instance) {
+      console.dir(instance);
+    });
+    callback(null, data);
+  }
+
+], function(error, results) {
+  if (error) {
+    if (error === 'moof') {
+      console.log('all required machines are started and running...')
+    } else {
+          console.log("waterfall error: " + error);
+    }
+  } else {
+    console.log("no waterfall error");
   }
 });
