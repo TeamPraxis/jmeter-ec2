0b72d4fe3582339c8fdb881c7cfc6a2aa927e45e
lyrobinson <lrobinson@teampraxis.com>
2014-03-14 11:27:14 -1000

    added creating ec2s and creating tags

--- ec2-helper-async.js
+++ ec2-helper-async.js
@@ -14,13 +14,58 @@ var ec2 = new AWS.EC2({region: 'us-west-2'}),
     startNum = 0,
     makeNum = 0;
 
-var startEC2 = function(instances, startNum, callback) {
+var startEC2 = function(instances, startNum, makeNum, callback) {
   var startInstances = _.chain(instances)
     .filter({state: 'stopped'})
     .pluck('id')
     .sample(startNum).value();
+
   console.log('startInstances = ' + startInstances);
-  ec2.startInstances({InstanceIds: startInstances}, callback);
+  if (startNum > 0) {
+    ec2.startInstances({InstanceIds: startInstances}, function(err, data) {
+      if (err) {
+        console.log('error when starting instances: ', err);
+      } else {
+        _.each(data.StartingInstances, function(instance) {
+          console.dir(instance);
+        });
+      }
+      callback(err, makeNum);
+    });
+  };
+  callback(null, makeNum);
+};
+
+var createEC2 = function(makeNum, callback) {
+  var params = {
+    ImageId: 'ami-f8bdd1c8',
+    MaxCount: makeNum,
+    MinCount: makeNum,
+    InstanceType: 't1.micro',
+    KeyName: 'Jenkins',
+    SecurityGroups: ['Simple-Web']
+  };
+  console.log('creating ' + makeNum + ' instances...');
+  if (makeNum > 0) {
+    ec2.runInstances(params, callback);
+  };
+};
+
+var createTags = function(instanceIds, callback) {
+  var params = {
+    Resources: instanceIds,
+    Tags: [
+      {
+        Key: 'Owner',
+        Value: 'jmetertest'
+      }
+    ]
+  };
+  console.log('params for createTags: ');
+  console.dir(params);
+  if (instanceIds.length > 0) {
+    ec2.createTags(params, callback);
+  };
 };
 
 async.waterfall([
@@ -43,31 +88,33 @@ async.waterfall([
     console.dir(instancesByState);
     if (reqNum <= ((instancesByState.running || 0) + (instancesByState.stopped || 0))) {
       if (reqNum <= (instancesByState.running || 0)) {
-        callback('moof', instances, startNum);
+        callback('moof', instances, startNum, makeNum);
     } else {
         console.log('need to start ' + (reqNum - (instancesByState.running || 0)) + ' more');
         startNum = reqNum - (instancesByState.running || 0);
-        callback(null, instances, startNum);
+        callback(null, instances, startNum, makeNum);
       }
     } else {
       console.log ('gotta start all the stopped ones and make ' + (reqNum - ((instancesByState.running || 0) + (instancesByState.stopped || 0))) + ' more');
       startNum = instancesByState.stopped || 0;
-      callback(null, instances, startNum);
+      makeNum = reqNum - ((instancesByState.running || 0) + (instancesByState.stopped || 0));
+      callback(null, instances, startNum, makeNum);
     }
     console.log('startNum: ' + startNum);
   },
   startEC2,
+  createEC2,
   function(data, callback) {
-    _.each(data.StartingInstances, function(instance) {
-      console.dir(instance);
-    });
-    callback(null, data);
-  }
+    var instanceIds = _.pluck(data.Instances, 'InstanceId');
+    console.log('created instances: ' + instanceIds);
+    callback(null, instanceIds);
+  },
+  createTags
 
 ], function(error, results) {
   if (error) {
     if (error === 'moof') {
-      console.log('all required machines are started and running...')
+      console.log('no action needed, machines are running...')
     } else {
           console.log("waterfall error: " + error);
     }
