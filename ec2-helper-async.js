e614da9da6f0c7931ef590f39c79b0ac3dde0b86
lyrobinson <lrobinson@teampraxis.com>
2014-03-17 13:37:16 -1000

    added valueOf?

--- ec2-helper-async.js
+++ ec2-helper-async.js
@@ -132,14 +132,14 @@ async.waterfall([
 ], function(error, results) {
   if (error) {
     if (error === 'moof') {
-      console.log(results);
-      return results;
+      console.log(results.valueOf());
+      return results.valueOf();
     } else {
           //console.log("waterfall error: " + error);
       return err;
     }
   } else {
-    console.log(results);
+    console.log(results.valueOf());
     return results;
   }
 });
