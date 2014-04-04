14b7adc05432a8a3715a34d6948e7afd0441b282
lyrobinson <lrobinson@teampraxis.com>
2014-04-04 13:06:10 -1000

    added name to results file instead of the repository name

--- jmeter-ec2.sh
+++ jmeter-ec2.sh
@@ -876,8 +876,8 @@ function runcleanup() {
     if [ -e "$project_home/$project-$DATETIME-noblanks.jtl" ] ; then rm $project_home/$project-$DATETIME-noblanks.jtl ; fi
     mkdir -p $project_home/results/
     mv $project_home/$project-$DATETIME-complete.jtl $project_home/results/
-    tail -n 1 $project_home/results/$project-$DATETIME-complete.jtl > $project_home/results/$PROJECT_NAME-${GIT_COMMIT:0:7}-$project-$DATETIME.jtl
-    sed '$ d' $project_home/results/$project-$DATETIME-complete.jtl >> $project_home/results/$PROJECT_NAME-${GIT_COMMIT:0:7}-$project-$DATETIME.jtl
+    tail -n 1 $project_home/results/$project-$DATETIME-complete.jtl > $project_home/results/$NAME-${GIT_COMMIT:0:7}-$project-$DATETIME.jtl
+    sed '$ d' $project_home/results/$project-$DATETIME-complete.jtl >> $project_home/results/$NAME-${GIT_COMMIT:0:7}-$project-$DATETIME.jtl
     rm $project_home/results/$project-$DATETIME-complete.jtl
 
     #***************************************************************************
